fb2c1609752bde812722f627c18892ef8ba34747
ThePotatoGamer0 <102364508+ThePotatoGamer0@users.noreply.github.com>
2026-06-24 03:32:23 +0100

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,15 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: "https://akira.potatogamer.uk",
   outDir: "dist",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(), 
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: { en: 'en' },
+      },
+    })
+  ],
   markdown: {
     shikiConfig: {
       theme: "github-light",
