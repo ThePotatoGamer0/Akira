625ba23ba532c5451b5fc0f6e62a06f19ac1a861
ThePotatoGamer0 <102364508+ThePotatoGamer0@users.noreply.github.com>
2026-06-24 03:40:53 +0100

    revert to manual sitemap.xml

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,19 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: "https://akira.potatogamer.uk",
   outDir: "dist",
-  integrations: [
-    mdx(), 
-    sitemap({
-      i18n: {
-        defaultLocale: 'en',
-        locales: { en: 'en' },
-      },
-    })
-  ],
+  integrations: [mdx()], // Removed sitemap()
   markdown: {
     shikiConfig: {
       theme: "github-light",
