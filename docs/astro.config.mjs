11f67f2bc3ea80c5bd74167a45b5ed4f41e21be7
ThePotatoGamer0 <102364508+ThePotatoGamer0@users.noreply.github.com>
2026-06-24 02:16:25 +0100

    fix code block formatting

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://akira.local",
   outDir: "dist",
+  integrations: [mdx()],
   markdown: {
     shikiConfig: {
       theme: "github-light",
